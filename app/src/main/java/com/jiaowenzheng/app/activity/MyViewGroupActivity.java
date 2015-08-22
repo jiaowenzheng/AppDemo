2007647fa7065cdf841e2f8b79a7dcf55f2b61e1
jiaowenzheng <ghd2000@126.com>
2015-08-22 11:45:16 +0800

    增加绘制view

---
+++ app/src/main/java/com/jiaowenzheng/app/activity/MyViewGroupActivity.java
@@ -0,0 +1,21 @@
+package com.jiaowenzheng.app.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.jiaowenzheng.app.R;
+
+/**
+ * Created by wenzheng on 2015/8/22.
+ */
+public class MyViewGroupActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_group);
+
+
+
+    }
+}
